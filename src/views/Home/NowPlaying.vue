21112f246f8146363c212761b6335626561442d8
wangming <1603657728@qq.com>
2020-04-16 20:38:39 +0800

    2-3 命名路由（使用路由name进行跳转）

--- src/views/Home/NowPlaying.vue
+++ src/views/Home/NowPlaying.vue
@@ -20,7 +20,11 @@
     methods: {
       toProduct(id) {
         // 编程式导航 跳转到 detail 页，并携带参数 id
-        this.$router.push(`/detail/${id}`)
+        // 路径跳转
+        // this.$router.push(`/detail/${id}`)
+
+        // 使用路由 name 跳转
+        this.$router.push({ name: 'Detail', params: { productid: 1234 }})
       }
     }
   }
